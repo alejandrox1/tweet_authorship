0ac6b9d7c1e569690227d1b86f7e962eb6b174dc
alarcj <alarcj137@gmail.com>
2017-02-24 19:16:55 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+consumer_key    = 'sFSeQNdkcLCEPUi81p4ClN3kC'
+consumer_secret = 'O9tec0ETYkyn0p1km6hYUQa2akkz9tlB6QAMu2nLOij0UbNBRa' 
+access_token    = '265463749-7uUERK4V6BZT8lEhVKlCTJfHOWfCUxEJaYuI3ysj'
+access_secret   = 'jWuskeWJTs8DxSSXgsQlXPPXajVrkqVqN6HNm2n3pnVWI'
+
